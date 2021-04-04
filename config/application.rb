efe0d9c97b96e677ffc5883021865ec59276c3c2
Dmitry Badichan <badimalex@gmail.com>
2021-04-04 17:37:01 +0300

    Add ads api

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,9 @@
 class Application < Sinatra::Base
+  helpers Validations
+
   configure do
     register Sinatra::Namespace
+    register ApiErrors
 
     set :app_file, File.expand_path('../config.ru', __dir__)
   end
@@ -9,9 +12,5 @@ class Application < Sinatra::Base
     register Sinatra::Reloader
 
     set :show_exceptions, false
-  end
-
-  get '/' do
-    json page_size: Settings.pagination.page_size
   end
 end
